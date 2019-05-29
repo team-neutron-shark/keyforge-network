5df5c72499c445bd1aff5b5f94bb2b7147977570
sigma <sigmaseven@gmail.com>
2019-05-29 00:38:05 -0500

    removed events from this file to add to event.go

--- observer.go
+++ observer.go
@@ -1,7 +1,5 @@
 package kfnetwork
 
-import "net"
-
 type Observer interface {
 	Notify(Event)
 }
@@ -10,26 +8,4 @@ type Subject interface {
 	AddObserver(Observer)
 	RemoveObserver(Observer)
 	NotifyObservers(Event)
-}
-
-type Event interface {
-	GetClient() *net.Conn
-	GetPacket() *Packet
-}
-
-type BaseEvent struct {
-	connection *net.Conn
-	packet     *Packet
-}
-
-type NetworkEvent struct {
-	BaseEvent
-}
-
-func (b BaseEvent) GetClient() *net.Conn {
-	return b.connection
-}
-
-func (b BaseEvent) GetPacket() *Packet {
-	return b.packet
 }
